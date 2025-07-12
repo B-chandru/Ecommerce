6a54bfa7dfbaab48c6696ee0e4dd7b1c0752dfed
B-chandru <thidrkavikanchandru@gmail.com>
2025-07-12 20:17:08 +0530

    added some info

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('We are under Development Mode!')
+    res.send('We are under Development Mode!.............')
 })
 
 app.listen(port, () => {
