3d1b65aec7326b937f66a0f0be25dd72481e9511
B-chandru <thidrkavikanchandru@gmail.com>
2025-07-12 22:10:18 +0530

    added some text

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("We are under Development Mode!.............");
+    res.send("We are under Development Mode!.");
 });
 
 const server = app.listen(3000, () => {
